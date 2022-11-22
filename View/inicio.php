2cbcf46ab13657cc44766a22f9eb54ec2bb45c11
BryanCachimuel <bryanloyo56@gmail.com>
2022-11-21 22:59:11 -0500

    creando al vista con la que iniciara el proyecto

---
+++ View/inicio.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tienda de Zapatos</title>
+    <link rel="stylesheet" href="Resources/css/materialize.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col m12">
+                <div class="card blue z-depth-4 white-text center-align">
+                    <h2>Tienda de Zapatos</h2>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
