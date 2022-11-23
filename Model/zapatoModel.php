e0d9a904b7067754285b70230ed8b7c85f3aea4b
BryanCachimuel <bryanloyo56@gmail.com>
2022-11-22 19:47:01 -0500

    creando el constructor que accede a la clase donde se definio la conexión hacia la base de datos

--- Model/zapatoModel.php
+++ Model/zapatoModel.php
@@ -1,5 +1,15 @@
 <?php
+
     class zapatoModel{
+
+        public $connect; // cadena de conexión para la bdd
         
+        public function __construct(){
+            try {
+                $this->connect = conexion::conectar(); // se hace el llamado a la clase conexion y se utiliza su método
+            } catch (Exception $e) {
+                die($e->getMessage());
+            }
+        }
     }
 ?>
