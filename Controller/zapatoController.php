033e757c9fe0f82526660efc44a67d1df5240cec
BryanCachimuel <bryanloyo56@gmail.com>
2022-11-22 19:49:37 -0500

    creando el constructor donde se hace el llamado a una instancia de la clase creada en el model

--- Controller/zapatoController.php
+++ Controller/zapatoController.php
@@ -1,5 +1,14 @@
 <?php
+
+    include_once("Model/zapatoModel.php");
+
     class zapatoController{
+
+        public $Model;
+
+        public function __construct(){
+            $this->Model = new zapatoModel();
+        }
 
         public function index(){
             include_once("View/inicio.php");
