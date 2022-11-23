b223e90fa63dfd6497299787beb3f20d9d7d7639
BryanCachimuel <bryanloyo56@gmail.com>
2022-11-22 19:37:56 -0500

    conexión hacia la base de datos

--- Config/conexion.php
+++ Config/conexion.php
@@ -1,5 +1,14 @@
 <?php
     class conexion{
         
+        public static function conectar(){
+           try {
+                $con = new PDO("mysql:host:localhost; dbname=tienda_zapatos; charset=utf8","root","");
+                $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);// se activan los siguientes atributos
+                return $con;
+           } catch (Exception $e) {
+                echo "Error en la conexión hacia la Base de Datos: ". $e->getMessage();
+           }
+        }
     }
 ?>
