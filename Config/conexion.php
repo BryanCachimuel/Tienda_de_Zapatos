99f980079fca763cfa6024cf416028b711f2c530
BryanCachimuel <bryanloyo56@gmail.com>
2022-11-23 19:58:33 -0500

    archivo de conexión hacia la base de datos

--- Config/conexion.php
+++ Config/conexion.php
@@ -2,8 +2,8 @@
     class conexion{
         
         public static function conectar(){
-           try {
-                $con = new PDO("mysql:host:localhost; dbname=tienda_zapatos; charset=utf8","root","");
+            try {
+                $con = new PDO("mysql:host=localhost; dbname=tienda_zapatos; charset=utf8","root","");
                 $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);// se activan los siguientes atributos
                 return $con;
            } catch (Exception $e) {
