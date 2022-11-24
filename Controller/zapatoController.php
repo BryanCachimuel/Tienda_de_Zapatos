f9cf965e6926eb76b7ee4165dfa354d582c52ae8
BryanCachimuel <bryanloyo56@gmail.com>
2022-11-23 22:37:08 -0500

    consumiendo el método de crearZapatos del model hacia el método creado en el controlador

--- Controller/zapatoController.php
+++ Controller/zapatoController.php
@@ -13,6 +13,24 @@
         public function index(){
             include_once("View/inicio.php");
         }
+
+        public function crear(){
+            include_once("View/guardar.php");
+        }
+
+        public function crearZapatos(){
+            $cz = new zapatoModel();
+            $cz->color = $_POST['txtColor'];
+            $cz->estilo = $_POST['cmbEstilo'];
+            $cz->genero = $_POST['cmbGenero'];
+            $cz->talla = $_POST['cmbTalla'];
+            $cz->cantidad = $_POST['txtCantidad'];
+            $cz->precio = $_POST['txtPrecio'];
+            $cz->valor_total = $_POST['txtValor_total'];
+
+            $this->Model->crearZapatos($cz);
+            header("Location:index.php");
+        }
     }
 
 ?>
