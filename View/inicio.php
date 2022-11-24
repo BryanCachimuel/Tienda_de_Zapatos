9dc91e5bbb8df9c60c432143b2845706f62f4d1e
BryanCachimuel <bryanloyo56@gmail.com>
2022-11-23 20:06:28 -0500

    creando la vista principal del proyecto

--- View/inicio.php
+++ View/inicio.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tienda de Zapatos</title>
     <link rel="stylesheet" href="Resources/css/materialize.css">
+    <link rel="stylesheet" href="Resources/css/estilos.css">
 </head>
 <body>
     <div class="container">
@@ -14,6 +15,33 @@
                 <div class="card blue z-depth-4 white-text center-align">
                     <h2>Tienda de Zapatos</h2>
                 </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col m12">
+                <a href="?c=crear" id="boton" class="btn btn-success z-depth-3">Registrar Zapatos</a>
+                <table class="table-responsive z-depth-3">
+                    <tr class="black">
+                        <th class="white-text center-align">Color</th>
+                        <th class="white-text center-align">Estilo</th>
+                        <th class="white-text center-align">Género</th>
+                        <th class="white-text center-align">Talla</th>
+                        <th class="white-text center-align">Cantidad</th>
+                        <th class="white-text center-align">Precio</th>
+                        <th class="white-text center-align">Valor Total</th>
+                    </tr>
+                    <?php foreach($this->Model->listarZapatos() as $lz) : ?>
+                        <tr>
+                            <td class="center-align"><?php echo $lz->color; ?></td>
+                            <td class="center-align"><?php echo $lz->estilos; ?></td>
+                            <td class="center-align"><?php echo $lz->generos; ?></td>
+                            <td class="center-align"><?php echo $lz->tallas; ?></td>
+                            <td class="center-align"><?php echo $lz->cantidad; ?></td>
+                            <td class="center-align">$<?php echo $lz->precio; ?></td>
+                            <td class="center-align">$<?php echo $lz->valor_total; ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </table>
             </div>
         </div>
     </div>
