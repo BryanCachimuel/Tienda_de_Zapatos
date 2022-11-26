488cb5f32c8d6c677452feb542a9df28bdc1e156
BryanCachimuel <bryanloyo56@gmail.com>
2022-11-25 19:34:43 -0500

    creando un nuevo registro de zapatos y control de la compra total

--- Model/zapatoModel.php
+++ Model/zapatoModel.php
@@ -23,7 +23,10 @@
 
         public function listarZapatos(){
             try{
-                $query = "SELECT z.id_zapato, z.color, z.precio, z.cantidad, z.valor_total, e.estilos, t.tallas, g.generos FROM zapato z INNER JOIN estilo e on z.id_estilo = e.id_estilo INNER JOIN talla t on z.id_talla = t.id_talla INNER JOIN genero g on z.id_genero = g.id_genero";
+                $query = "SELECT z.id_zapato, z.color, z.precio, z.cantidad, z.valor_total, e.estilos, t.tallas, g.generos 
+                          FROM zapato z INNER JOIN estilo e on z.id_estilo = e.id_estilo 
+                          INNER JOIN talla t on z.id_talla = t.id_talla 
+                          INNER JOIN genero g on z.id_genero = g.id_genero";
                 $smt = $this->con->prepare($query);
                 $smt->execute();
                 return $smt->fetchAll(PDO::FETCH_OBJ); // se hace el retorno de un objeto
@@ -68,7 +71,13 @@
         public function crearZapatos(zapatoModel $data){
             try {
                 $query = "INSERT INTO zapato(color,cantidad,precio,valor_total,id_estilo,id_genero,id_talla) VALUES(?,?,?,?,?,?,?)";
-                $this->con->prepare($query)->execute(array($data->color, $data->cantidad, $data->precio, $data->valor_total, $dat->id_estilo, $data->id_genero, $data->id_talla));
+                $this->con->prepare($query)->execute(array($data->color, 
+                                                           $data->cantidad, 
+                                                           $data->precio, 
+                                                           $data->valor_total = $data->cantidad * $data->precio, 
+                                                           $data->id_estilo, 
+                                                           $data->id_genero, 
+                                                           $data->id_talla));
             } catch (Exception $e) {
                 die($e->getMessage());
             }
