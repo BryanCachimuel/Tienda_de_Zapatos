18b9ec1965cedcca6818821cc4c1e9052b887c92
BryanCachimuel <bryanloyo56@gmail.com>
2022-11-26 20:17:47 -0500

    creando la función para consumir la función de actualización creada en el model

--- Controller/zapatoController.php
+++ Controller/zapatoController.php
@@ -36,6 +36,20 @@
             header("Location:index.php");
         }
 
+        public function editarZapatos(){
+            $cz = new zapatoModel();
+            $cz->id_zapato = $_POST['txtId'];
+            $cz->color = $_POST['txtColor'];
+            $cz->cantidad = $_POST['txtCantidad'];
+            $cz->precio = $_POST['txtPrecio'];
+            $cz->id_estilo = $_POST['cmbEstilo'];
+            $cz->id_genero = $_POST['cmbGenero'];
+            $cz->id_talla = $_POST['cmbTalla'];
+
+            $this->Model->actualizarZapato($cz);
+            header("Location:index.php");
+        }
+
         public function editar(){
             $cz = new zapatoModel();
             if(isset($_REQUEST['id'])){
