b7de96bfdfa1e4f3ed84dd3f82a2b23c2f199dd2
BryanCachimuel <bryanloyo56@gmail.com>
2022-11-27 22:33:58 -0500

    agregando en los formualarios el boton con la funcionalidad de cancelar

--- View/editar.php
+++ View/editar.php
@@ -82,7 +82,8 @@
             <div class="row">
                 <div class="col m3"></div>
                 <div class="col m6">
-                    <input type="submit" name="btnGuardar" class="btn red z-depth-3" value="Actualizar">
+                    <input type="submit" name="btnGuardar" class="btn green z-depth-3" value="Actualizar">
+                    <a href="?c=regresar" class="btn red z-depth-3">Cancelar</a>
                 </div>
                 <div class="col m3"></div>
             </div>
--- View/guardar.php
+++ View/guardar.php
@@ -81,7 +81,8 @@
             <div class="row">
                 <div class="col m3"></div>
                 <div class="col m6">
-                    <input type="submit" name="btnGuardar" class="btn red z-depth-3" value="Crear">
+                    <input type="submit" name="btnGuardar" class="btn green z-depth-3" value="Crear">
+                    <a href="?c=regresar" class="btn red z-depth-3">Cancelar</a>
                 </div>
                 <div class="col m3"></div>
             </div>
